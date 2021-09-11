6315a5590250201987fa57374436d8a18d458ba5
Asfaqur-Alam <ahsansagor449@gmail.com>
2021-09-11 11:17:30 +0600

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,13 @@
-console.log('Clint side  javascript file loaded')
-
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-    })
-})
+// fetch('http://localhost:3000/weather?address=!').then((response) => {
+//     response.json().then((data) => {
+//         if (data.error) {
+//             console.log(data.error)
+//         } else {
+//             console.log(data.location)
+//             console.log(data.forecast)
+//         }
+//     })
+// })
 
 const WeatherForm = document.querySelector('form')
 const search = document.querySelector('input')
@@ -20,8 +18,6 @@ messageOne.textContent = 'Loading.... '
 messagetwo.textContent= ''
 
 WeatherForm.addEventListener('submit', (e) => {
-
-
     e.preventDefault()
 
     const location = search.value
